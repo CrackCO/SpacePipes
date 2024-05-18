63d76ee3d0accf4a820222165699e9f80753ff30
ndgde <ivanbujvol5@gmail.com>
2024-05-18 12:51:24 +0300

    added view and projection matrices into the Camera, added getters and setters for all fields, refactoring done

--- include/Camera.hpp
+++ include/Camera.hpp
@@ -6,10 +6,47 @@ class Camera {
 private:
     glm::vec3 pos;
     glm::vec3 dir;
+    glm::vec3 up;
+    glm::mat4 viewMatrix;
+
+    float fov;
+    float aspect;
+    float nearPlane;
+    float farPlane;
+    glm::mat4 projectionMatrix;
 
 public:
-    Camera(const glm::vec3& pos, const glm::vec3& dir);
+    Camera::Camera(
+        const glm::vec3& pos, const glm::vec3& dir, const glm::vec3& up, 
+        float fov, float aspect, float nearPlane, float farPlane
+    );
 
-    glm::vec3 getPos() const;
-    glm::vec3 getDir() const;
+    ~Camera();
+
+    glm::vec3 getPos    () const;
+    glm::vec3 getDir    () const;
+    glm::vec3 getUp     () const;
+    glm::mat4 getMatrix () const;
+
+
+    float     getFov              () const;
+    float     getAspect           () const;
+    float     getNearPlane        () const;
+    float     getFarPlane         () const;
+    glm::mat4 getProjectionMatrix () const;
+
+
+    void setPos ( const glm::vec3& newPos );
+    void setDir ( const glm::vec3& newDir );
+    void setUp  ( const glm::vec3& newUp  );
+
+    void updateViewMatrix();
+
+
+    void setFov       ( float newFov       );
+    void setAspect    ( float newAspect    );
+    void setNearPlane ( float newNearPlane );
+    void setFarPlane  ( float newFarPlane  );
+
+    void updateProjectionMatrix();
 };
--- src/Camera.cpp
+++ src/Camera.cpp
@@ -1,15 +1,53 @@
 #include "Camera.hpp"
 
-Camera::Camera(const glm::vec3& pos, const glm::vec3& dir)
+Camera::Camera(
+    const glm::vec3& pos, const glm::vec3& dir, const glm::vec3& up, 
+    float fov, float aspect, float nearPlane, float farPlane
+)
     :
-    pos(pos),
-    dir(dir)
-{}
+    pos ( pos ),
+    dir ( dir ),
+    up  ( up  ),
 
-glm::vec3 Camera::getPos() const {
-    return pos;
+    fov       ( fov       ),
+    aspect    ( aspect    ),
+    nearPlane ( nearPlane ),
+    farPlane  ( farPlane  )
+{
+    updateViewMatrix();
+    updateProjectionMatrix();
 }
 
-glm::vec3 Camera::getDir() const {
-    return dir;
+~Camera() {}
+
+
+glm::vec3 Camera::getPos   () const { return pos;        }
+glm::vec3 Camera::getDir   () const { return dir;        }
+glm::vec3 Camera::getUp    () const { return up;         }
+glm::mat4 Camera::getMatrix() const { return viewMatrix; }
+
+void Camera::setPos ( const glm::vec3& newPos ) { pos = newPos; updateViewMatrix(); }
+void Camera::setDir ( const glm::vec3& newDir ) { dir = newDir; updateViewMatrix(); }
+void Camera::setUp  ( const glm::vec3& newUp  ) { up  = newUp;  updateViewMatrix(); }
+
+void Camera::updateViewMatrix()
+{
+    viewMatrix = glm::lookAt(pos, pos + dir, up);
+}
+
+
+float     Camera::getFov              () const { return fov;              }
+float     Camera::getAspect           () const { return aspect;           }
+float     Camera::getNearPlane        () const { return nearPlane;        }
+float     Camera::getFarPlane         () const { return farPlane;         }
+glm::mat4 Camera::getProjectionMatrix () const { return projectionMatrix; }
+
+void Camera::setFov       ( float newFov       ) { fov       = newFov;       updateProjectionMatrix(); }
+void Camera::setAspect    ( float newAspect    ) { aspect    = newAspect;    updateProjectionMatrix(); }
+void Camera::setNearPlane ( float newNearPlane ) { nearPlane = newNearPlane; updateProjectionMatrix(); }
+void Camera::setFarPlane  ( float newFarPlane  ) { farPlane  = newFarPlane;  updateProjectionMatrix(); }
+
+void Camera::updateProjectionMatrix()
+{
+    projectionMatrix = glm::perspective(glm::radians(fov), aspect, nearPlane, farPlane);
 }
